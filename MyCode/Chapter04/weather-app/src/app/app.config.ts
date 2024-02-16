0ced3d5f9062f63967486acf8e01a0e53bea2927
Gregg Carey <gregg.carey@outlook.com>
2024-02-16 14:35:44 -0500

    add angular pwa

--- MyCode/Chapter04/weather-app/src/app/app.config.ts
+++ MyCode/Chapter04/weather-app/src/app/app.config.ts
@@ -1,7 +1,11 @@
 import { provideHttpClient } from '@angular/common/http';
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideAnimationsAsync(), provideHttpClient()]
+  providers: [provideAnimationsAsync(), provideHttpClient(), provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+    })]
 };
